57a6b60bc3f2dde07ba5a652e22a45fb68dfdf49
FelixLeChat <felixlrc21@gmail.com>
2015-11-28 04:38:05 -0500

    Add multithread on arduino

---
+++ Arduino/arduino_client/arduino_client.ino
@@ -0,0 +1,225 @@
+#include <pt.h>
+
+// set pin numbers:
+// Enable the selection on another Decoder
+const int DecoderEnabled =  12;
+// Adresse sur le bus pour les décodeurs
+const int AdresseBus0 = 4;
+const int AdresseBus1 = 3;
+const int AdresseBus2 = 2;
+// Adresse pour choisir les décodeurs
+const int AdresseDecoder0 = 6;
+const int AdresseDecoder1 = 5;
+// Controle des transistors pour grounder les étages
+const int Etage0 = 7;
+const int Etage1 = 8;
+const int Etage2 = 9;
+const int Etage3 = 10;
+const int Etage4 = 11;
+
+// Constantes et variables
+const int delayus = 100;
+int temps = 200;
+long n = 0;
+boolean randomBool = true;
+boolean pluie = true;
+boolean UpState[5][5][5] ={
+{
+  {false, false, false, false, false},
+  {false, false, false, false, false},
+  {false, false, false, false, false},
+  {false, false, false, false, false},
+  {false, false, false, false, false} },
+{
+  {false, false, false, false, false},
+  {false, false, false, false, false},
+  {false, false, false, false, false},
+  {false, false, false, false, false},
+  {false, false, false, false, false} },
+{
+  {false, false, false, false, false},
+  {false, false, false, false, false},
+  {false, false, false, false, false},
+  {false, false, false, false, false},
+  {false, false, false, false, false} },
+{
+  {false, false, false, false, false},
+  {false, false, false, false, false},
+  {false, false, false, false, false},
+  {false, false, false, false, false},
+  {false, false, false, false, false} },
+{
+  {false, false, false, false, false},
+  {false, false, false, false, false},
+  {false, false, false, false, false},
+  {false, false, false, false, false},
+  {false, false, false, false, false} } 
+};
+
+int minutes = 0;
+int heures = 0;
+
+//-------------------------------------------------------------------------------------------//
+void setup() {
+  // set the digital pin as output:
+  pinMode(DecoderEnabled, OUTPUT);
+  pinMode(AdresseBus0, OUTPUT);
+  pinMode(AdresseBus1, OUTPUT);
+  pinMode(AdresseBus2, OUTPUT);
+  pinMode(AdresseDecoder0, OUTPUT);
+  pinMode(AdresseDecoder1, OUTPUT);
+  pinMode(Etage0, OUTPUT);
+  pinMode(Etage1, OUTPUT);
+  pinMode(Etage2, OUTPUT);
+  pinMode(Etage3, OUTPUT);
+  pinMode(Etage4, OUTPUT);
+}
+
+// Thread
+static struct pt pt1;
+
+// Loop principale---------------------------------------------------------------------------//
+void loop()
+{ 
+  UpState[0][0][0] = true;
+  while(true)
+  {
+    lightCube();
+  }
+}
+
+// Éteindre tout-----------------------------------------------------------------------------//
+void resetLights(){
+     digitalWrite(Etage0, LOW); digitalWrite(Etage1, LOW); digitalWrite(Etage2, LOW); digitalWrite(Etage3, LOW); digitalWrite(Etage4, LOW);
+}
+
+// Remise à false de l'étage-----------------------------------------------------------------------------//
+void resetUpState(int z){
+
+  for(int x = 0 ; x < 5 ; x++){
+   for(int y = 0 ; y < 5 ; y++){
+    UpState[x][y][z] = false;
+   } 
+  }
+}
+
+//-------------------------------------------------------------------------------------------//
+void lightCube(){
+  
+  for( int z = 0; z < 5 ; z++){
+   for( int x = 0; x < 5 ; x++){
+     for( int y = 0 ; y < 5 ; y++){
+       if(UpState[x][y][z]){
+         lightDEL(x,y,z);
+       }
+     }
+   } 
+  }
+}
+
+//-------------------------------------------------------------------------------------------//
+void lightDEL(int x, int y, int z){
+  
+ digitalWrite(DecoderEnabled, HIGH);
+  
+ switch(x){
+  
+   case 0:
+     digitalWrite(AdresseDecoder0, LOW); digitalWrite(AdresseDecoder1, LOW);
+     
+     switch(y){
+       case 0:
+        digitalWrite(AdresseBus0, LOW); digitalWrite(AdresseBus1, LOW); digitalWrite(AdresseBus2, LOW); break;
+       case 1:
+        digitalWrite(AdresseBus0, HIGH); digitalWrite(AdresseBus1, LOW); digitalWrite(AdresseBus2, LOW); break; 
+       case 2: 
+        digitalWrite(AdresseBus0, LOW); digitalWrite(AdresseBus1, HIGH); digitalWrite(AdresseBus2, LOW); break;
+       case 3:
+        digitalWrite(AdresseBus0, HIGH); digitalWrite(AdresseBus1, HIGH); digitalWrite(AdresseBus2, LOW); break;
+       case 4:
+        digitalWrite(AdresseBus0, LOW); digitalWrite(AdresseBus1, LOW); digitalWrite(AdresseBus2, HIGH); break;
+     }
+   break;
+   
+   case 1:
+     digitalWrite(AdresseDecoder0, LOW); digitalWrite(AdresseDecoder1, LOW);
+     
+     switch(y){
+       case 0:
+        digitalWrite(AdresseBus0, HIGH); digitalWrite(AdresseBus1, LOW); digitalWrite(AdresseBus2, HIGH); break;
+       case 1:
+        digitalWrite(AdresseBus0, LOW); digitalWrite(AdresseBus1, HIGH); digitalWrite(AdresseBus2, HIGH); break;  
+       case 2:
+        digitalWrite(AdresseBus0, HIGH); digitalWrite(AdresseBus1, HIGH); digitalWrite(AdresseBus2, HIGH); break;
+       case 3:
+        digitalWrite(AdresseDecoder0, HIGH); digitalWrite(AdresseBus0, LOW); digitalWrite(AdresseBus1, LOW); digitalWrite(AdresseBus2, LOW);break;
+       case 4:
+        digitalWrite(AdresseDecoder0, HIGH); digitalWrite(AdresseBus0, HIGH); digitalWrite(AdresseBus1, LOW); digitalWrite(AdresseBus2, LOW);break;
+     }
+   break;
+   
+   case 2:
+     digitalWrite(AdresseDecoder0, HIGH); digitalWrite(AdresseDecoder1, LOW);
+     
+     switch(y){
+       case 0:
+        digitalWrite(AdresseBus0, LOW); digitalWrite(AdresseBus1, HIGH); digitalWrite(AdresseBus2, LOW); break;
+       case 1:
+        digitalWrite(AdresseBus0, HIGH); digitalWrite(AdresseBus1, HIGH); digitalWrite(AdresseBus2, LOW); break;  
+       case 2:
+        digitalWrite(AdresseBus0, LOW); digitalWrite(AdresseBus1, LOW); digitalWrite(AdresseBus2, HIGH); break;   
+       case 3:
+        digitalWrite(AdresseBus0, HIGH); digitalWrite(AdresseBus1, LOW); digitalWrite(AdresseBus2, HIGH); break;
+       case 4:
+        digitalWrite(AdresseBus0, LOW); digitalWrite(AdresseBus1, HIGH); digitalWrite(AdresseBus2, HIGH); break;
+     }
+   break;
+   
+   case 3:
+     digitalWrite(AdresseDecoder0, LOW); digitalWrite(AdresseDecoder1, HIGH);
+     
+     switch(y){
+      case 0:
+        digitalWrite(AdresseDecoder0, HIGH); digitalWrite(AdresseDecoder1, LOW); digitalWrite(AdresseBus0, HIGH); digitalWrite(AdresseBus1, HIGH); digitalWrite(AdresseBus2, HIGH); break;
+       case 1:
+        digitalWrite(AdresseDecoder0, HIGH); digitalWrite(AdresseBus0, LOW); digitalWrite(AdresseBus1, LOW); digitalWrite(AdresseBus2, LOW); break; 
+       case 2:
+        digitalWrite(AdresseBus0, LOW); digitalWrite(AdresseBus1, LOW); digitalWrite(AdresseBus2, LOW); break; 
+       case 3:
+        digitalWrite(AdresseBus0, HIGH); digitalWrite(AdresseBus1, LOW); digitalWrite(AdresseBus2, LOW); break;
+       case 4:
+        digitalWrite(AdresseBus0, LOW); digitalWrite(AdresseBus1, HIGH); digitalWrite(AdresseBus2, LOW); break;
+     }
+   break;
+   
+   case 4:
+     digitalWrite(AdresseDecoder0, LOW); digitalWrite(AdresseDecoder1, HIGH);
+     
+     switch(y){  
+       case 0:
+        digitalWrite(AdresseBus0, HIGH); digitalWrite(AdresseBus1, HIGH); digitalWrite(AdresseBus2, LOW); break;
+       case 1:
+        digitalWrite(AdresseBus0, LOW); digitalWrite(AdresseBus1, LOW); digitalWrite(AdresseBus2, HIGH); break;
+       case 2: 
+        digitalWrite(AdresseBus0, HIGH); digitalWrite(AdresseBus1, LOW); digitalWrite(AdresseBus2, HIGH); break;
+       case 3:
+        digitalWrite(AdresseBus0, LOW); digitalWrite(AdresseBus1, HIGH); digitalWrite(AdresseBus2, HIGH); break;
+       case 4:
+        digitalWrite(AdresseBus0, HIGH); digitalWrite(AdresseBus1, HIGH); digitalWrite(AdresseBus2, HIGH); break;
+     }
+  break;
+ } 
+
+ // grounder une étage selon z
+  switch(z){
+  
+   case 0: digitalWrite(Etage0, HIGH); break;
+   case 1: digitalWrite(Etage1, HIGH); break;
+   case 2: digitalWrite(Etage2, HIGH); break;
+   case 3: digitalWrite(Etage3, HIGH); break;
+   case 4: digitalWrite(Etage4, HIGH); break;
+ } 
+ 
+ delayMicroseconds(delayus);
+ resetLights();
+}
--- Client/index.js
+++ Client/index.js
@@ -1,3 +1,23 @@
+var SerialPort = require("serialport").SerialPort;
+
+var serialPort = new SerialPort("/dev/ttyACM0", {
+  baudrate: 9600
+});
+
+serialPort.on("open", function () {
+  console.log('open');
+
+  /*serialPort.on('data', function(data) {
+    console.log('data received: ' + data);
+  });*/
+
+  serialPort.write(new Buffer('4','ascii'), function(err, results) {
+    console.log('err ' + err);
+    console.log('results ' + results);
+  });
+});
+
+
 var ws = require("nodejs-websocket");
 var wsConnected = ws.connect("ws://websocket-nodejs.herokuapp.com", function(wss)
 {
@@ -10,25 +30,10 @@ wsConnected.on('connect', function() {
 	wsConnected.on('text', function incoming(message) {
 		// when text is received from Websocket Server
 		console.log('received: %s', message);
+
+		serialport.write(new Buffer('4','0,0,0'), function(err, results) {
+		    console.log('err ' + err);
+		    console.log('results ' + results);
+		});
 	});
-});
-
-
-var SerialPort = require("serialport").SerialPort;
-
-var serialPort = new SerialPort("/dev/ttyACM0", {
-  baudrate: 9600
-});
-
-serialPort.on("open", function () {
-  console.log('open');
-
-  serialPort.on('data', function(data) {
-    console.log('data received: ' + data);
-  });
-
-  serialPort.write(new Buffer('4','ascii'), function(err, results) {
-    console.log('err ' + err);
-    console.log('results ' + results);
-  });
 });
