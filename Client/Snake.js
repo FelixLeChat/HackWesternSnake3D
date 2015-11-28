0395c5173cfc7193b6666feb0ff6680bce3f3654
antonbks <anton.buksa@queensu.ca>
2015-11-28 18:37:41 -0500

    python 2.0

--- Client/Snake.js
+++ Client/Snake.js
@@ -188,7 +188,7 @@ function isHittingItself()
 	return false;
 }
 
-/*pyshell.on(function(err){
-	//if(err) throw err;
+pyshell.on(function(err){
+	if(err) throw err;
 	console.log("finished Python script");
-});*/
+});
