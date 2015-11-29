2a13e04e612cb7450f1f13bee7cd36a8dcd2baa2
Maxime P <mpperusse@gmail.com>
2015-11-29 00:17:29 -0500

    lel

--- index.js
+++ index.js
@@ -1,25 +1,74 @@
-var WebSocketServer = require("ws").Server
-var http = require("http")
-var express = require("express")
-var app = express()
-var port = process.env.PORT || 5000
+window.onload = function() {
 
-app.use(express.static(__dirname + "/"))
-
-var server = http.createServer(app)
-server.listen(port)
-
-console.log("http server listening on %d", port)
-
-var wss = new WebSocketServer({server: server})
-console.log("websocket server created")
+  // Get references to elements on the page.
+  var form = document.getElementById('message-form');
+  var messageField = document.getElementById('message');
+  var messagesList = document.getElementById('messages');
+  var socketStatus = document.getElementById('status');
+  var closeBtn = document.getElementById('close');
 
 
-wss.on('connection', function connection(ws) {
+  // Create a new WebSocket.
+  var socket = new WebSocket('ws://websocket-nodejs.herokuapp.com');
 
-	ws.on('message', function incoming(message) 
-	{
 
-	});
+  // Handle any errors that occur.
+  socket.onerror = function(error) {
+    console.log('WebSocket Error: ' + error);
+  };
 
-});
+
+  // Show a connected message when the WebSocket is opened.
+  socket.onopen = function(event) {
+    socketStatus.innerHTML = 'Connected to: ws://websocket-nodejs.herokuapp.com';
+    socketStatus.className = 'open';
+  };
+
+
+  // Handle messages sent by the server.
+  socket.onmessage = function(event) {
+    var message = event.data;
+    messagesList.innerHTML += '<li class="received"><span>Received:</span>' +
+                               message + '</li>';
+  };
+
+
+  // Show a disconnected message when the WebSocket is closed.
+  socket.onclose = function(event) {
+    socketStatus.innerHTML = 'Disconnected from WebSocket.';
+    socketStatus.className = 'closed';
+  };
+
+
+  // Send a message when the form is submitted.
+  form.onsubmit = function(e) {
+    e.preventDefault();
+
+    // Retrieve the message from the textarea.
+    var message = messageField.value;
+
+    // Send the message through the WebSocket.
+    socket.send(message);
+
+    // Add the message to the messages list.
+    messagesList.innerHTML += '<li class="sent"><span>Sent:</span>' + message +
+                              '</li>';
+
+    // Clear out the message field.
+    messageField.value = '';
+
+    return false;
+  };
+
+
+  // Close the WebSocket connection when the close button is clicked.
+  closeBtn.onclick = function(e) {
+    e.preventDefault();
+
+    // Close the WebSocket.
+    socket.close();
+
+    return false;
+  };
+
+};
