c39e1fb36689fc1ba3b16630f58a0900802f4523
Maxime P <mpperusse@gmail.com>
2015-11-28 23:49:26 -0500

    lel

--- index.js
+++ index.js
@@ -90,49 +90,62 @@ var results = {up:0, down:0, left:0, right:0, forward:0, backward:0};
 
 wss.on('connection', function connection(ws) {
 
-  ws.on('message', function incoming(message) 
-  {
-      var received = true;
-      message = message.toLowerCase();
+  	// Handle any errors that occur.
+	ws.onerror = function(error) {
+		console.log('WebSocket Error: ' + error);
+	};
 
-      switch(message)
-      {
-        case "up":
-            results.up++;
-            break;
-        case "down":
-            results.down++;
-            break;
-        case "left":
-            results.left++;
-            break;
-        case "right":
-            results.right++;
-            break;
-        case "forward":
-            results.forward++;
-            break;
-        case "backward":
-            results.backward++;
-            break;
-        default:
-            received = false;
-            break;
-      }
 
-        if(received)
-        {
-            ws.send("vote compiled");
-            wss.clients.forEach(function each(client) {
-                client.send("Vote for : " + message);
-            });
-        }
-        else
-            ws.send("vote not compiled : " + message + " is invalid");
-  });
+	// Show a connected message when the WebSocket is opened.
+	ws.onopen = function(event) {
+		socketStatus.innerHTML = 'Connected to: ws://echo.websocket.org';
+		socketStatus.className = 'open';
+	};
 
-    ws.send('Connected to voting channel');
-    ws.send("Choices : up, down, left, right, forward, backward");
+
+	// Handle messages sent by the server.
+	ws.onmessage = function(event) {
+	var message = event.data;
+		messagesList.innerHTML += '<li class="received"><span>Received:</span>' + message + '</li>';
+	};
+
+
+	// Show a disconnected message when the WebSocket is closed.
+	ws.onclose = function(event) {
+		socketStatus.innerHTML = 'Disconnected from WebSocket.';
+		socketStatus.className = 'closed';
+	};
+
+	// Send a message when the form is submitted.
+	form.onsubmit = function(e) {
+		e.preventDefault();
+
+		// Retrieve the message from the textarea.
+		var message = messageField.value;
+
+		// Send the message through the WebSocket.
+		ws.send(message);
+
+		// Add the message to the messages list.
+		messagesList.innerHTML += '<li class="sent"><span>Sent:</span>' + message +
+		                          '</li>';
+
+		// Clear out the message field.
+		messageField.value = '';
+
+		return false;
+	};
+
+
+	// Close the WebSocket connection when the close button is clicked.
+	closeBtn.onclick = function(e) {
+		e.preventDefault();
+
+		// Close the WebSocket.
+		ws.close();
+
+		return false;
+	};
 
 });
 
