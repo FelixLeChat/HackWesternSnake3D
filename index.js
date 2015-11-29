a1724a6875002c09ab768faccbda837273dfba0f
Maxime P <mpperusse@gmail.com>
2015-11-29 02:29:29 -0500

    x

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ console.log("http server listening on %d", port)
 var wss = new WebSocketServer({server: server})
 console.log("websocket server created")
 
-var results = {up:0, down:0, left:0, right:0, forward:0, backward:0, message};
+var results = {up:0, down:0, left:0, right:0, forward:0, backward:0, message:""};
 
 wss.on('connection', function connection(ws) {
 
